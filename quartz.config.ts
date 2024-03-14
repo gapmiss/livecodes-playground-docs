a24f7c508da4b0334cc0fed9f9532dec387e4d71
gapmiss <gapmiss@proton.me>
2024-03-14 11:29:01 -0400

    Quartz sync: Mar 14, 2024, 11:29 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -54,7 +54,7 @@ const config: QuartzConfig = {
       Plugin.CreatedModifiedDate({
         priority: ["frontmatter", "filesystem"],
       }),
-      Plugin.Latex({ renderEngine: "katex" }),
+      // Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting({
         theme: {
           light: "github-light",
@@ -62,7 +62,7 @@ const config: QuartzConfig = {
         },
         keepBackground: false,
       }),
-      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: true }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents({maxDepth:6}),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest", openLinksInNewTab: false }),
