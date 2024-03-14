a010912b8cd8b6a0b242029d75d2509cb60eac24
gapmiss <gapmiss@proton.me>
2024-03-13 21:29:17 -0400

    Quartz sync: Mar 13, 2024, 9:29 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "livecodes-playground",
+    pageTitle: "Livecodes playground",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
