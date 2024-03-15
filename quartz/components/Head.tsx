850e8742cf77fb23f0c1c7688353771657f300d4
gapmiss <gapmiss@proton.me>
2024-03-15 12:01:43 -0400

    Quartz sync: Mar 15, 2024, 12:01 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -15,7 +15,8 @@ export default (() => {
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
-    const iconPath = joinSegments(baseDir, "static/icon.png")
+    // const iconPath = joinSegments(baseDir, "static/icon.png")
+    const iconPath = joinSegments(baseDir, "static/favicon.svg")
     const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
