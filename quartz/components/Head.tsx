89d385d6153bc623b1e49589ad87310c943b1a5e
gapmiss <gapmiss@proton.me>
2024-03-16 15:24:42 -0400

    Quartz sync: Mar 16, 2024, 3:24 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -13,46 +13,35 @@ export default (() => {
 
     const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
     const path = url.pathname as FullSlug
-    const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
-
-    // const iconPath = joinSegments(baseDir, "static/icon.png")
-    // const iconPath = joinSegments(baseDir, "static/icon.svg")
+    // const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
     const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
     const favIcon = `https://${cfg.baseUrl}/static/favicon.ico`
-    // const icon = `https://${cfg.baseUrl}/static/icon.svg`
     const appleTouchIcon = `https://${cfg.baseUrl}/static/apple-touch-icon.png`
     const webManifest = `https://${cfg.baseUrl}/static/site.webmanifest`
+    const favicon32 = `https://${cfg.baseUrl}/static/favicon-32x32.png`
+    const favicon16 = `https://${cfg.baseUrl}/static/favicon-16x16.png`
     return (
       <head>
         <title>Livecodes Playground docs - {title}</title>
         <meta charSet="utf-8" />
-        {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
+        {/*cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
           </>
-        )}
+        )*/}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
-        <meta property="og:width" content="1200" />
-        <meta property="og:height" content="675" />
-
+        {/* {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />} */}
+        {/* <meta property="og:width" content="1200" /> */}
+        {/* <meta property="og:height" content="675" /> */}
         <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+        <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
         <link rel="manifest" href={webManifest} />
-
-
-
         <link rel="icon" href={favIcon} sizes="32x32" />
-        {/* <link rel="icon" href={icon} type="image/svg+xml" /> */}
-        {/* <link rel="apple-touch-icon" href={appleTouchIcon} /> */}
-        {/* <link rel="icon" href={iconPath} /> */}
-        {/* <link rel="manifest" href={webManifest} /> */}
-
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
         {css.map((href) => (
